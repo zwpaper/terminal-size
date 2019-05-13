7ff92018c272f5c59d1b46077a9ac2d88d7390f4
zwPapEr <zw.paper@gmail.com>
2019-05-13 14:40:34 +0800

    Fallback to use system env if STDOUT is a tty and no size found
    When using eshell(shell inside Emacs), STDOUT is a tty, but (0, 0)
    is returned by terminal_size and stty.

--- src/unix.rs
+++ src/unix.rs
@@ -1,6 +1,6 @@
 extern crate libc;
 use std::os::raw::*;
-use std::os::unix::io::RawFd;
+use std::env;
 
 use super::{Height, Width};
 
@@ -22,17 +22,27 @@ struct WinSize {
 /// Returns the size of the terminal defaulting to STDOUT, if available.
 ///
 /// If STDOUT is not a tty, returns `None`
+/// If STDOUT is a tty, but both width and height is 0,
+/// fallback to use system env: COLUMNS and LINES.
 pub fn terminal_size() -> Option<(Width, Height)> {
-    use self::libc::STDOUT_FILENO;
-    terminal_size_using_fd(STDOUT_FILENO)
+    let size = terminal_size_using_fd();
+    match size {
+        Some((Width(0), Height(0))) => {
+            terminal_size_using_env()
+        },
+        _ => size,
+    }
 }
 
 /// Returns the size of the terminal using the given file descriptor, if available.
 ///
-/// If the given file descriptor is not a tty, returns `None`
-pub fn terminal_size_using_fd(fd: RawFd) -> Option<(Width, Height)> {
+/// If the STDOUT file descriptor is not a tty, returns `None`
+pub fn terminal_size_using_fd() -> Option<(Width, Height)> {
+    use self::libc::STDOUT_FILENO;
     use self::libc::ioctl;
     use self::libc::isatty;
+
+    let fd = STDOUT_FILENO;
     let is_tty: bool = unsafe { isatty(fd) == 1 };
 
     if !is_tty {
@@ -60,16 +70,35 @@ pub fn terminal_size_using_fd(fd: RawFd) -> Option<(Width, Height)> {
         (rows as u16, cols as u16)
     };
 
-    if rows > 0 && cols > 0 {
-        Some((Width(cols), Height(rows)))
-    } else {
+    Some((Width(cols), Height(rows)))
+}
+
+/// Returns the size of the terminal using system env:
+/// COLUMNS and LINES
+///
+/// If both env are 0, returns `None`
+fn terminal_size_using_env() -> Option<(Width, Height)> {
+    let get_u16_from_env = |x: &str| -> u16 {
+        if let Some(v) = env::var_os(x) {
+            v.into_string().unwrap()
+                .parse::<u16>().unwrap()
+        } else {
+            0
+        }
+    };
+    let c = get_u16_from_env("COLUMNS");
+    let r = get_u16_from_env("LINES");
+
+    if r == 0 && c == 0 {
         None
+    } else {
+        return Some((Width(c), Height(r)))
     }
 }
 
 #[test]
-/// Compare with the output of `stty size`
-fn compare_with_stty() {
+/// Compare using_fd with the output of `stty size`
+fn compare_using_fd_with_stty() {
     use std::process::Command;
     use std::process::Stdio;
 
@@ -97,6 +126,78 @@ fn compare_with_stty() {
     let rows = u16::from_str_radix(data.next().unwrap(), 10).unwrap();
     let cols = u16::from_str_radix(data.next().unwrap(), 10).unwrap();
     println!("{}", stdout);
+    println!("{} {}", rows, cols);
+
+    if let Some((Width(w), Height(h))) = terminal_size_using_fd() {
+        assert_eq!(rows, h);
+        assert_eq!(cols, w);
+    }
+}
+
+#[test]
+/// Compare using env result with the output of `tput cols` and `tput lines`
+fn compare_using_env_with_tput() {
+    use std::process::Command;
+    use std::process::Stdio;
+
+    let cols_output = Command::new("tput")
+        .arg("cols")
+        .stderr(Stdio::inherit())
+        .output()
+        .unwrap();
+    let rows_output = Command::new("tput")
+        .arg("lines")
+        .stderr(Stdio::inherit())
+        .output()
+        .unwrap();
+    assert!(cols_output.status.success());
+    assert!(rows_output.status.success());
+
+    let cols = String::from_utf8(cols_output.stdout).unwrap()
+        .trim()
+        .parse::<u16>()
+        .unwrap();
+    let rows = String::from_utf8(rows_output.stdout).unwrap()
+        .trim()
+        .parse::<u16>()
+        .unwrap();
+
+    println!("{} {}", rows, cols);
+
+    if let Some((Width(w), Height(h))) = terminal_size_using_env() {
+        assert_eq!(rows, h);
+        assert_eq!(cols, w);
+    }
+}
+
+#[test]
+/// Compare result with the output of `tput cols` and `tput lines`
+fn compare_with_tput() {
+    use std::process::Command;
+    use std::process::Stdio;
+
+    let cols_output = Command::new("tput")
+        .arg("cols")
+        .stderr(Stdio::inherit())
+        .output()
+        .unwrap();
+    let rows_output = Command::new("tput")
+        .arg("lines")
+        .stderr(Stdio::inherit())
+        .output()
+        .unwrap();
+    assert!(cols_output.status.success());
+    assert!(rows_output.status.success());
+
+    let cols = String::from_utf8(cols_output.stdout).unwrap()
+        .trim()
+        .parse::<u16>()
+        .unwrap();
+    let rows = String::from_utf8(rows_output.stdout).unwrap()
+        .trim()
+        .parse::<u16>()
+        .unwrap();
+
     println!("{} {}", rows, cols);
 
     if let Some((Width(w), Height(h))) = terminal_size() {
